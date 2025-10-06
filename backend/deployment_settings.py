bfb1aa6117c3a9ea52e97c4f6e44c4049c53c58b
Yousaf <khanyousafamin@gmail.com>
2025-10-06 19:49:17 +0500

    Finaly

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -8,6 +8,7 @@ ALLOWED_HOSTS=[os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS=['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 
+
 DEBUG=True
 SECRET_KEY=os.environ.get('SECRET_KEY')
 
@@ -28,6 +29,22 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "https://render-deploy-authentication-system-react.onrender.com",
 ]
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_EXPOSE_HEADERS = ['Set-Cookie']
+
+
 
 
 STORAGES={
@@ -52,9 +69,9 @@ DATABASES={
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_PORT = 465  
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
--- backend/settings.py
+++ backend/settings.py
@@ -239,15 +239,16 @@ REST_FRAMEWORK = {
 }
 
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=False)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 #CORS_ALLOW_ALL_ORIGINS = True
